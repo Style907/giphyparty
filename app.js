e88ab35d56f1e05f35cc0265989fb539323b9665
Logan Lanni <style907@Logans-Air.hsd1.or.comcast.net>
2022-12-04 20:51:34 -0800

    removed console logs not needed

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ console.log("Let's get this party started!");
 async function getGif(search){
     let res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=0MUHmgFNg4i5KqLjkPmt51is0qgKO9Bu&q=${search}&limit=1&offset=0&rating=pg-13&lang=en`);
     let term = res.data.data
-    console.log(res)
-  console.log(term[0].images.downsized.url);
 
   $('.gifs').append(`<div style = "background-image: url( ${term[0].images.original.url} ); background-size: cover; height: 300px; width: 300px; display: inline-block;"></div> `)
 }
